48ebd57d07f383eb2de84cda277f5511d2e2433a
Yamanami-satoko <piyopiyo3105@gmail.com>
2024-07-13 10:39:31 +0900

    Add files via upload

---
+++ js/carusel.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".slider").slick({
+    arrows: false,
+    autoplay: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: "15%",
+    dots: true,
+  });
+});
